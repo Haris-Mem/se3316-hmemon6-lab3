d61a73aeb16fe9ef86296a6e28b2dd904f471bf7
Haris-Mem <90726020+Haris-Mem@users.noreply.github.com>
2022-11-04 15:49:02 -0400

    implementing Error

--- MusicApp/src/components/Loader.jsx
+++ MusicApp/src/components/Loader.jsx
@@ -5,7 +5,7 @@ const Loader = ({title}) => (
     <img src={loader} alt="loader" className='w-32 h-32 object-contain' /> 
     <h1 className='font-bold text-2x1 text-white mt-2'>
       {title || "Loading..."}</h1>
-    Loader
+
     
     </div>
 );
