48aa860b48a5712f8fd63e7b13b038081cf70285
Haris-Mem <90726020+Haris-Mem@users.noreply.github.com>
2022-11-04 17:40:23 -0400

    Modifying backend

--- MusicApp/src/components/PlayPause.jsx
+++ MusicApp/src/components/PlayPause.jsx
@@ -1,16 +1,17 @@
-import {FaPauseCircle, FaPlayCircle} from 'react-icons/fa';
+import React from 'react';
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
 
-const PlayPause = ({isPlaying, activeSong, song, handlePause, handlePlay}) => (isPlaying && activeSong?.title === song.title ? (
-  <FaPauseCircle 
-  size={35}
-  className="text-gray-300"
-  onClick={handlePause}
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) => (isPlaying && activeSong?.title === song.title ? (
+  <FaPauseCircle
+    size={35}
+    className="text-gray-300"
+    onClick={handlePause}
   />
 ) : (
-  <FaPlayCircle 
-  size={35}
-  className="text-gray-300"
-  onClick={handlePlay}
+  <FaPlayCircle
+    size={35}
+    className="text-gray-300"
+    onClick={handlePlay}
   />
 ));
 
--- MusicApp/src/redux/services/shazamCore.js
+++ MusicApp/src/redux/services/shazamCore.js
@@ -1,20 +1,32 @@
-import {createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
 
-    export const shazamCoreAPI = createApi({
-        reducerPath: 'shazamCoreApi',
-        baseQuery: fetchBaseQuery({
-            baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
-            prepareHeaders: (headers) =>{
-                headers.set('X-RapidAPI-Key', '3dd7129626msh279c82491011679p17ee54jsn159c4f5ec946');
-                return headers;
-            }
-        }),
-        endpoints: (builder) => ({
-            getTopCharts: builder.query({query: () => '/charts/world'}),
-        })
-    });
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+    getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+    getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+  }),
+});
 
-    export const{
-        useGetTopChartsQuery,
-    } = shazamCoreAPI;
+export const {
+  useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
+  useGetArtistDetailsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} = shazamCoreApi;
