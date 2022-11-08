bddf14dd2115a5b08eef64310e3f7265d8766be0
Haris-Mem <90726020+Haris-Mem@users.noreply.github.com>
2022-11-08 17:41:00 -0500

    Adding Parser

--- express-demo/parser.js
+++ express-demo/parser.js
@@ -1,16 +1,26 @@
-const parse = require('csv-parse');
+const csv = require('csv-parser');
 const fs = require('fs');
-const csvData = [];
 
-fs.createReadStream('Lab3_Data\genres.csv')
-    .pipe(
-        parse({
-            delimiter: ','
-        })
-    )
-    .on('data',function(dataRow){
-        csvData.push(dataRow);
-    })
-    .on('end',function(){
-        console.log(csvData);
-    })
+const genresCsvData = [];
+const albumCsvData = [];
+const artistCsvData = [];
+const tracksCsvData = [];
+
+fs.createReadStream("Lab3_Data/genres.csv").pipe(csv({})).on("data", (data) => genresCsvData.push(data));
+//console.log(genresCsvData);
+
+fs.createReadStream("Lab3_Data/raw_albums.csv")
+.pipe(csv({}))
+.on("data", (data) => albumCsvData.push(data));
+//console.log(albumCsvData);
+
+fs.createReadStream("Lab3_Data/raw_artists.csv")
+.pipe(csv({}))
+.on("data", (data) => artistCsvData.push(data));
+console.log(artistCsvData)
+
+fs.createReadStream("Lab3_Data/raw_tracks.csv")
+.pipe(csv({}))
+.on("data", (data) => tracksCsvData.push(data));
+//console.log(tracksCsvData)
+
